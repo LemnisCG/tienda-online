d8e32e45eaf27ac161aa42877864fff61ba9ea7a
JChigne <lemniscatachigne@gmail.com>
2024-09-14 05:10:07 -0300

    Barra de anuncio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
+import './styles/Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
-        
-    </nav>        
+    <>
+        <aside className='p-2 bg-light'>
+            <div className='container d-flex justify-content-between align-items-center '>
+                <p className="text-center mx-auto text-center flex-grow-1">Delivery Gratis en Santiago por $39.990</p>
+                <ul className="nav justify-content-end">
+                    <li className='nav-item'>
+                        <a className='nav-link' href="#">
+                            <img src="/assets/facebook.svg" alt='Facebook Icons'/>
+                        </a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='#'>
+                            <img src="/assets/instagram.svg" alt='Instagram Icons'/>
+                        </a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href="#">
+                            <img src="/assets/twitch.svg" alt='Twitch Icons'/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+        <nav className="navbar">
+
+        </nav>        
+    </>
   )
 }
 
